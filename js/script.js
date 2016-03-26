96f54dcbe2efc50e9014cd0babf990ac467b65e3
Rich Ramsland <rramsland@gmail.com>
2016-03-26 02:27:18 -0400

    Added Upgrade JS

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 	var score = 0;
+	var boxValue = 1;
+	var upgradeCost = 5;
 
 	function addScore(){
-		score += 1;
+		score += boxValue;
 		$(".score").html("$"+score);
+	}
+	function upgrade(){
+		if(score >= upgradeCost){
+			score-=upgradeCost;
+			$(".score").html("$"+score);
+			boxValue+=1;
+			$(".box").html("$"+boxValue);
+			upgradeCost*=2;
+			$(".upgrade").html("$"+upgradeCost);
+		}
+
 	}
